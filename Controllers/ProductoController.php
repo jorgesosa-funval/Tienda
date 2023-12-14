80a80bf4424398d63a05ec51f55b1ab4de10f66c
¨miguelamador23¨ <miguelamador23@outlook.com>
2023-12-13 22:47:37 -0600

    Funcion Destroy

--- Controllers/ProductoController.php
+++ Controllers/ProductoController.php
@@ -4,6 +4,8 @@ namespace Controller;
 
 use Models\Producto;
 
+use PDO;
+
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/Vendor/autoload.php';
 
@@ -18,7 +20,7 @@ class ProductoController
 
         return $data;
     }
-    
+
     // Mostrar un registro de la tabla
     public function show() //Mateus
     {
@@ -33,9 +35,19 @@ class ProductoController
     public function update() //Derek 
     {
     }
-    
+
     // Eliminar un registro de la tabla
-    public function destroy()//Miguel
+    public function destroy($id, $usuario, $contraseña)
     {
+        $conn = new PDO("mysql:host=localhost;dbname=tiendita", $usuario, $contraseña);
+        $sql = "DELETE FROM productos WHERE id = :id";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindValue(':id', $id);
+        $stmt->execute();
+        if ($stmt->rowCount() > 0) {
+            echo "Se eliminó el registro correctamente.";
+        } else {
+            echo "No se encontró el registro a eliminar.";
+        }
     }
 }
