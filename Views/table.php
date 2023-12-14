e6e0361ae4b696ddea8d5fffb28cf56790141d82
Alan-Britoo <rotcarrillo_8@hotmail.com>
2023-12-14 09:22:36 -0500

    tabla

---
+++ Views/table.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/table.css">
+    <title>Tabla</title>
+</head>
+<body class="bod" >
+    <table class="table" >
+        <thead class="nav">
+            <tr  class="nav">
+                <th class="id" >ID</th>
+                <th class="name" >Name</th>
+                <th class="email" >Email</th>
+                <th class="name" >Role</th>
+                <th class="pp" >Profile Picture</th>
+                <th class="actions" >Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="celda" >
+                <td class="id" >1</td>
+                <td class="name" ></td>
+                <td class="email" ></td>
+                <td class="name" ></td>
+                <td class="pp"></td>
+                <td class="actions" >
+                    <button class="edit" >Edit</button>
+                    <button class="delete" >Delete</button>
+                </td>
+            </tr>
+            <tr class="celda" >
+                <td class="id" >2</td>
+                <td class="name" ></td>
+                <td class="email" ></td>
+                <td class="name" ></td>
+                <td class="pp"></td>
+                <td class="actions" >
+                    <button class="edit" >Edit</button>
+                    <button class="delete" >Delete</button>
+                </td>
+            </tr>
+            <tr class="celda" >
+                <td class="id" >3</td>
+                <td class="name" ></td>
+                <td class="email" ></td>
+                <td class="name" ></td>
+                <td class="pp" ></td>
+                <td class="actions" >
+                    <button class="edit" >Edit</button>
+                    <button class="delete" >Delete</button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
